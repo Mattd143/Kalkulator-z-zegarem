8ffed6b8ca7abac3b55942a8c54ed4b58e8a1d08
Mati <Mati@MATI-KOMPUTER>
2025-04-04 12:17:02 +0200

    Działa zegar

--- Kalkulator_z_zegarem/Kalkulator_z_zegarem/Form2.Designer.cs
+++ Kalkulator_z_zegarem/Kalkulator_z_zegarem/Form2.Designer.cs
@@ -40,11 +40,99 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.godzina_minuta = new System.Windows.Forms.Label();
+            this.data = new System.Windows.Forms.Label();
+            this.dzien = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tarcza = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.tarcza)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // godzina_minuta
+            // 
+            this.godzina_minuta.AutoSize = true;
+            this.godzina_minuta.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.godzina_minuta.Location = new System.Drawing.Point(162, 34);
+            this.godzina_minuta.Name = "godzina_minuta";
+            this.godzina_minuta.Size = new System.Drawing.Size(249, 91);
+            this.godzina_minuta.TabIndex = 0;
+            this.godzina_minuta.Text = "label1";
+            this.godzina_minuta.UseWaitCursor = true;
+            // 
+            // data
+            // 
+            this.data.AutoSize = true;
+            this.data.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.data.Location = new System.Drawing.Point(209, 179);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(138, 51);
+            this.data.TabIndex = 1;
+            this.data.Text = "label2";
+            // 
+            // dzien
+            // 
+            this.dzien.AutoSize = true;
+            this.dzien.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dzien.Location = new System.Drawing.Point(254, 274);
+            this.dzien.Name = "dzien";
+            this.dzien.Size = new System.Drawing.Size(138, 51);
+            this.dzien.TabIndex = 2;
+            this.dzien.Text = "label3";
+            this.dzien.Click += new System.EventHandler(this.dzien_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PeachPuff;
+            this.button1.Location = new System.Drawing.Point(171, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(328, 66);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Zmień sposób wyświetlania zegara";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tarcza
+            // 
+            this.tarcza.Location = new System.Drawing.Point(187, 34);
+            this.tarcza.Name = "tarcza";
+            this.tarcza.Size = new System.Drawing.Size(388, 314);
+            this.tarcza.TabIndex = 4;
+            this.tarcza.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Peru;
+            this.ClientSize = new System.Drawing.Size(679, 467);
+            this.Controls.Add(this.tarcza);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dzien);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.godzina_minuta);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tarcza)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label godzina_minuta;
+        private Label data;
+        private Label dzien;
+        private Button button1;
+        private PictureBox tarcza;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Kalkulator_z_zegarem/Kalkulator_z_zegarem/Form2.cs
+++ Kalkulator_z_zegarem/Kalkulator_z_zegarem/Form2.cs
@@ -12,6 +12,20 @@ namespace Kalkulator_z_zegarem
 {
     public partial class Form2 : Form
     {
+
+
+        int cx;
+        int cy;
+        System.Windows.Forms.Timer t = new System.Windows.Forms.Timer();
+
+        Bitmap bmp;
+        Graphics g;
+        int secHand = 140;
+        int minHand = 110;
+        int hourHand = 80;
+        int width = 300;
+        int height = 300;
+
         public Form2()
         {
             InitializeComponent();
@@ -23,6 +37,118 @@ namespace Kalkulator_z_zegarem
             InitializeComponent();
             this.kalkulator = rodzic;
             Form2.loo++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            godzina_minuta.Visible = !godzina_minuta.Visible;
+            data.Visible = !data.Visible;
+            dzien.Visible = !dzien.Visible;
+            tarcza.Visible = !tarcza.Visible;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            //deklaracja rozpoczecia pracy timera, ustawienie kordów bitmapy i środka zegara
+            timer1.Start();
+            bmp = new Bitmap(Width + 1, Height + 1);
+            cx = width / 2;
+            cy = height / 2;
+            tarcza.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            godzina_minuta.Text = DateTime.Now.ToString("HH:mm:ss");
+
+            data.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            dzien.Text = DateTime.Now.ToString("dddd");
+            //rysowansko
+            g = Graphics.FromImage(bmp);
+            //pobranie czasu
+            int ss = DateTime.Now.Second;
+            int mm = DateTime.Now.Minute;
+            int hh = DateTime.Now.Hour;
+
+            int[] handcoord = new int[2];
+            //czysczenie mapy i wypelnianie jej kolorem, każdy tik zegara czyści pictureboxa i rysuje na nowo tarcze ze wskazowkami
+            g.Clear(Color.Peru);
+            //rysowanie tarczy
+            g.DrawEllipse(new Pen(Color.Black, 1f), 0, 0, 300, 300);
+
+            Font font = new Font("Arial", 12);
+            Brush brush = Brushes.Black;
+
+            // Rysowanie cyfr, wykorzystanie trygonometrii 
+            for (int i = 1; i <= 12; i++)
+            {
+                double angle = Math.PI / 6 * (i - 3); // Przesunięcie, bo 12 = 0° na górze
+                int x = (int)(cx + secHand * Math.Cos(angle)) - 10;
+                int y = (int)(cy + secHand * Math.Sin(angle)) - 10;
+                g.DrawString(i.ToString(), font, brush, new PointF(x, y));
+            }
+
+
+            //wskazowka sekund
+            handcoord = msCoord(ss, secHand);
+            g.DrawLine(new Pen(Color.Red, 1f), new Point(cx, cy), new Point(handcoord[0], handcoord[1]));
+            //wskazowka minut
+            handcoord = msCoord(mm, minHand);
+            g.DrawLine(new Pen(Color.Blue, 2f), new Point(cx, cy), new Point(handcoord[0], handcoord[1]));
+            //wskazówka godzin
+            handcoord = hrCoord(hh % 12, mm, hourHand);
+            g.DrawLine(new Pen(Color.Black, 3f), new Point(cx, cy), new Point(handcoord[0], handcoord[1]));
+            //załadowanie bitmapy do pictureboxa
+            tarcza.Image = bmp;
+            this.Text = "Aktualny czas-" + hh + ":" + mm + ":" + ss;
+            //zwolnienie pamieci
+            g.Dispose();
+        }
+
+        // kordy na wskazowki minuty i sekundy
+        private int[] msCoord(int val, int hlen)
+        {
+            int[] coord = new int[2];
+            //kazda sekunda to 6 stopni
+            val *= 6;
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        //kordy na wskazowke godziny
+        private int[] hrCoord(int hval, int mval, int hlen)
+        {
+            int[] coord = new int[2];
+
+            //1 godzina to 30 stopni, 1 minuta to 0.5 stopni
+            int val = (int)((hval * 30) + (mval * 0.5));
+
+            if (val >= 0 && val <= 180)
+            {
+                coord[0] = cx + (int)(hlen * Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            else
+            {
+                coord[0] = cx - (int)(hlen * -Math.Sin(Math.PI * val / 180));
+                coord[1] = cy - (int)(hlen * Math.Cos(Math.PI * val / 180));
+            }
+            return coord;
+        }
+
+        private void dzien_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
